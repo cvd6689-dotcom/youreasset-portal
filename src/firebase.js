85e4d46eedc262bfe01a133bb803bc2de71c875f
cvd6689-dotcom <cvd6689@gmail.com>
2026-03-24 16:24:37 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDh88w68TZGwabggyWRS7vCc7VTEiXHTo",
+  apiKey: "AIzaSyDh88w68TZGwabggyvWRS7vCc7VTEiXHTo",
   authDomain: "yoursasset.firebaseapp.com",
   projectId: "yoursasset",
   storageBucket: "yoursasset.firebasestorage.app",
   messagingSenderId: "148918167461",
-  appId: "1:148918167461:web:60552be8d70e0160d66a4c",
+  appId: "1:148918167461:web:6d552be8d70e0160d66a4c",
+  measurementId: "G-NLXZXPXHV2"
 };
 
 const app = initializeApp(firebaseConfig);
